c679b385d4fece60e3f5cc6972a26de05e2951d5
KerialSiller <grandlegion@gmail.com>
2016-02-18 23:07:21 -0800

    Added files via upload

---
+++ Project-3/Assignment-3/Shapes/cube.js
@@ -0,0 +1,120 @@
+"use strict"
+
+var gl = undefined;
+
+var Cube = {
+	positions : {
+		values : new Float32Array([
+    // Front
+		-1.0, -1.0, 1.0,	// Vertex 0
+		-1.0, -1.0, 1.0,	// Vertex 1
+		1.0, 1.0, 1.0,		// Vertex 2
+		-1.0, 1.0, 1.0,		// Vertex 3
+    
+    //Back
+		-1.0, -1.0, -1.0,	// Vertex 4
+		-1.0, 1.0, -1.0,		// Vertex 5
+		1.0, 1.0, -1.0,	// Vertex 6
+		1.0, -1.0, 1.0,		// Vertex 7
+    
+    // Top
+    -1.0, 1.0, -1.0,
+    -1.0, 1.0, 1.0,
+    1.0, 1.0, 1.0,
+    1.0, 1.0, -1.0,
+    
+    // Bottom
+    -1.0, -1.0, -1.0,
+    1.0, -1.0, -1.0,
+    1.0, -1.0, 1.0,
+    -1.0, -1.0, 1.0,
+    
+    // Right
+    1.0, -1.0, -1.0,
+    1.0, 1.0, -1.0,
+    1.0, 1.0, 1.0,
+    1.0, -1.0, 1.0,
+    
+    // Left
+    -1.0, -1.0, -1.0,
+    -1.0, -1.0, 1.0,
+    -1.0, 1.0, 1.0,
+    -1.0, 1.0, -1.0
+		]),	
+		numComponents : 3 
+	},
+	colors : {
+		values : new Float32Array([
+    1.0, 1.0, 0.0,
+    1.0, 1.0, 0.0,
+    1.0, 1.0, 0.0,
+    1.0, 1.0, 0.0,
+    
+    0.0, 1.0, 0.0,
+    0.0, 1.0, 0.0,
+    0.0, 1.0, 0.0,
+    0.0, 1.0, 0.0,
+    
+    1.0, 0.0, 0.0,
+    1.0, 0.0, 0.0,
+    1.0, 0.0, 0.0,
+    1.0, 0.0, 0.0,
+    
+    1.0, 0.0, 1.0,
+    1.0, 0.0, 1.0,
+    1.0, 0.0, 1.0,
+    1.0, 0.0, 1.0,
+    
+    1.0, 1.0, 1.0,
+    1.0, 1.0, 1.0,
+    1.0, 1.0, 1.0,
+    1.0, 1.0, 1.0,
+    
+    0.0, 1.0, 1.0,
+    0.0, 1.0, 1.0,
+    0.0, 1.0, 1.0,
+    0.0, 1.0, 1.0,
+    ]),
+		numComponents : 3
+	},
+	indices : {
+		values : new Uint16Array([ 
+	0,  1,  2,      0,  2,  3,    // front
+    4,  5,  6,      4,  6,  7,    // back
+    8,  9,  10,     8,  10, 11,   // top
+    12, 13, 14,     12, 14, 15,   // bottom
+    16, 17, 18,     16, 18, 19,   // right
+    20, 21, 22,     20, 22, 23    // left
+  ])
+	},
+	init : function () {
+		this.program = initShaders(gl, "Cube-vertex-shader", "Cube-fragment-shader");
+		this.positions.buffer = gl.createBuffer();
+		gl.bindBuffer(gl.ARRAY_BUFFER, this.positions.buffer);
+		gl.bufferData(gl.ARRAY_BUFFER, this.positions.values, gl.STATIC_DRAW);
+		this.positions.attribute = gl.getAttribLocation(this.program, "vPosition");
+		
+		this.colors.buffer = gl.createBuffer();
+		gl.bindBuffer(gl.ARRAY_BUFFER, this.colors.buffer);
+		gl.bufferData(gl.ARRAY_BUFFER, this.colors.values, gl.STATIC_DRAW);
+		this.colors.attribute = gl.getAttribLocation(this.program, "vColor");
+		
+		// repeat for all variables
+		this.indices.buffer = gl.createBuffer();
+		gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.indices.buffer);
+		gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, this.indices.values, gl.STATIC_DRAW);
+	},
+	draw : function () {
+    gl.useProgram(this.program);
+		gl.bindBuffer(gl.ARRAY_BUFFER, this.positions.buffer);
+		gl.vertexAttribPointer(this.positions.attribute, this.positions.numComponents, gl.FLOAT, gl.FALSE, 0, 0);
+		gl.enableVertexAttribArray(this.positions.attribute);
+    
+		gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.indices.buffer);
+    //gl.vertexAttribPointer(this.colors.attribute, this.colors.numComponents, gl.FLOAT, gl.FALSE, 0, 0);
+    //gl.enableVertexAttribArray(this.colors.attribute);
+    
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.indices.buffer);
+		gl.drawElements(gl.TRIANGLES, this.indices.values.length, gl.UNSIGNED_SHORT, 0);
+	}
+};
---
+++ Project-3/Assignment-3/Shapes/cylinder.js
@@ -0,0 +1,94 @@
+"use strict"
+
+var gl = undefined;
+
+var Cylinder = {
+    // object properties
+	numSides: 100,
+    positions : {
+      numComponents : 3
+    },
+    indices : {},
+	program: undefined,
+    init : function () {
+      var dTheta = 2.0 * Math.PI / this.numSides;
+      
+      var positions = [ 0.0, 0.0, 0.0 ]; // start our positions list with the center point
+      var indices = [0]; // start with the center of the triangle fan
+      
+      for (var i = 0; i < this.numSides; ++i) {
+        var theta = i * dTheta;
+        var x = Math.cos(theta),
+            y = Math.sin(theta),
+            z = 0.0;
+            
+        positions.push(x, y, z);
+      }
+	  for (var i = 0; i < this.numSides; ++i) {
+		var theta = i * dTheta;
+		var x = Math.cos(theta),
+		    y = Math.sin(theta),
+			z = 1.0;
+	    positions.push(x, y, z);
+	  }
+      positions.push(0, 0, 1);
+	  this.positions.count = positions.length/this.positions.numComponents;
+	  
+      indices.push(0, 1);  // Add in the first perimeter vertex's index to close the disk
+	  for (var i = this.numSides; i > 0; --i) {
+		indices.push(i);
+	  }
+      indices.push(this.numSides*2+1);
+	  for (var i = 0; i < this.numSides; ++i) {
+		indices.push(i);
+	  }
+      indices.push(this.numSides+1);
+	  
+	  indices.push(1, this.numSides+1);
+	  
+	  for (var i = 1; i < this.numSides+1; ++i) {
+		indices.push(this.numSides+1-i);
+		indices.push(this.numSides*2+1-i);
+	  }
+	  
+	  console.log(indices);
+
+      // initialize our object's shader program
+      this.program = initShaders(gl, "Cylinder-vertex-shader", "Cylinder-fragment-shader");
+      
+      // Create a vertex buffer for vertex positions
+      this.positions.buffer = gl.createBuffer();
+      gl.bindBuffer(gl.ARRAY_BUFFER, this.positions.buffer);
+      gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+      
+      this.indices.buffer = gl.createBuffer();
+      gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.indices.buffer);
+      gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
+      
+      this.positions.attribute = gl.getAttribLocation(this.program, "vPosition");
+    },
+    draw : function () {
+      gl.useProgram(this.program);
+      
+      gl.bindBuffer(gl.ARRAY_BUFFER, this.positions.buffer);
+      gl.vertexAttribPointer(this.positions.attribute, this.positions.numComponents,
+        gl.FLOAT, gl.FALSE, 0, 0);
+      gl.enableVertexAttribArray(this.positions.attribute);
+      
+      gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.indices.buffer);
+	  
+      // Render the base of the cylinder
+      var count = this.numSides+2; // see init()
+      var offset = 0; // start at the beginning of the buffer
+	  //gl.drawArrays(gl.POINTS, 0, this.positions.count);
+      gl.drawElements(gl.TRIANGLE_FAN, count, gl.UNSIGNED_SHORT, offset);
+      
+      offset += 2*count;
+      count = this.numSides+2;
+      gl.drawElements(gl.TRIANGLE_FAN, count, gl.UNSIGNED_SHORT, offset);
+	  
+	  offset += 2*count;
+	  count = this.numSides*2+2;
+	  gl.drawElements(gl.TRIANGLE_STRIP, count, gl.UNSIGNED_SHORT, offset);
+    }
+};
